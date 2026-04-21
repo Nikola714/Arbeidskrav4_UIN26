38db056a04b5948194cc11473906c2aac27e0934
Nikola714 <nikolamp@hiof.no>
2026-04-21 12:09:24 +0200

    leget til riktig prosjekt id

--- backend/sanity/client.js
+++ backend/sanity/client.js
@@ -1,7 +1,7 @@
 import {createClient} from "@sanity/client";
 
 export const client = createClient({
-    projectId: "29k2pp07",
+    projectId: "739xtgnl",
     dataset: "production",
     apiVersion: "v2025-04-21",
     useCdn: true

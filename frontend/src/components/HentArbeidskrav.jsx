383da6e3b49e7dc3d20c596042e5d9952d0d004b
tapperisa-wq <tapperisa@hotmail.com>
2026-04-21 15:54:56 +0200

    hente fra sanity

--- frontend/src/components/HentArbeidskrav.jsx
+++ frontend/src/components/HentArbeidskrav.jsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react"
+import client from '../sanity/client'
 
 
 export default function Hentarbeidskrav() {
 
+    const [sanityKrav, setSanityKrav] = useState([])
+
+    useEffect(()=>{
+        async function fetchAllArbeidskrav() {
+            const alleArbeidskrav = await client.fetch(`*[_type == "arbeidskrav"]{
+                _id,
+                arbeidskravnavn,
+                beskrivelse
+                }`)
+            setSanityKrav(alleArbeidskrav)
+        }
+        fetchAllArbeidskrav()
+    },[])
+
+    console.log(sanityKrav)
 
 
     return(
-
+        <section id="arbeidskravliste">
+            {sanityKrav.map((ak) => (
+                <article key={ak._id}>
+                    <h2>{ak.arbeidskravnavn}</h2>
+                    <p>{ak.beskrivelse}</p>
+                </article>
+        ))}
+        </section>
     )
 }

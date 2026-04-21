963a5aa635ecddb680c7a7496d536c56d8f881d4
tapperisa-wq <tapperisa@hotmail.com>
2026-04-21 14:10:15 +0200

    endre

--- frontend/src/components/Isabel.jsx
+++ frontend/src/components/Isabel.jsx
@@ -9,7 +9,7 @@ export default function Isabel(){
     <h1>Isabel Tapper</h1>
     <a href="mailto:istap9887@hiof.no">istap9887@hiof.no</a>
     <h2>Studie: Enkeltemner</h2>
-    <h4>(Starter Informasjonssystemer høsten 2026)</h4>
+    <h5>(Starter Informasjonssystemer høsten 2026)</h5>
     </article>
     </>    
     )

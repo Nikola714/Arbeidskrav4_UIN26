cd3e93f55829ce779ff8ca0ac09e977fafd50a8c
tapperisa-wq <tapperisa@hotmail.com>
2026-04-21 12:29:16 +0200

    components mappe
    legge til components

---
+++ frontend/src/components /Amanda.jsx
@@ -0,0 +1,14 @@
+export default function Amanda() {
+
+    return (
+        <>
+        <img src="../Bilder/amanda.jpg" alt="bilde av Amanda Torstensen"/>
+        <h2>Amanda Torstensen</h2> 
+        <a href="mailto:amanda.torstensen@gmail.com">mail</a>
+        <h3>Studie: Digitale medier og Design</h3>
+        <h4>Andreårs student</h4> 
+        </>
+    )}
+
+
+// Problemer med pc, så jeg har selv programmert alt men noen ganger måtte andre pushe opp for meg. 

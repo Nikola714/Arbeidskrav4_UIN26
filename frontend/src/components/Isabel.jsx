a12d05c37545e069d10c922dbf847e7ed2ea13d8
tapperisa-wq <tapperisa@hotmail.com>
2026-04-23 10:42:14 +0200

    gjøre litt ryddig

--- frontend/src/components/Isabel.jsx
+++ frontend/src/components/Isabel.jsx
@@ -5,11 +5,11 @@ export default function Isabel(){
     return(
     <>
     <article className="isabelKort">
-    <img src="bilder/isabel.JPG" alt="portrettIsabel"></img>
-    <h1>Isabel Tapper</h1>
-    <a href="mailto:istap9887@hiof.no">istap9887@hiof.no</a>
-    <h2>Studie: Enkeltemner</h2>
-    <h5>(Starter Informasjonssystemer høsten 2026)</h5 >
+        <img src="bilder/isabel.JPG" alt="portrettIsabel"></img>
+        <h1>Isabel Tapper</h1>
+        <a href="mailto:istap9887@hiof.no">istap9887@hiof.no</a>
+        <h2>Studie: Enkeltemner</h2>
+        <h5>(Starter Informasjonssystemer høsten 2026)</h5 >
     </article>
     </>    
     )

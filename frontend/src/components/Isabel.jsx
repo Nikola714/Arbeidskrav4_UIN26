cf2a1e57f4851b1bbf2ba1da202fce20dfc1e4cc
tapperisa-wq <tapperisa@hotmail.com>
2026-04-23 10:59:11 +0200

    kommentarer

--- frontend/src/components/Isabel.jsx
+++ frontend/src/components/Isabel.jsx
@@ -5,8 +5,9 @@ export default function Isabel(){
     return(
     <>
     <article className="isabelKort">
-        <img src="bilder/isabel.JPG" alt="portrettIsabel"></img>
         <h1>Isabel Tapper</h1>
+        <img src="bilder/isabel.JPG" alt="portrettIsabel"></img>
+        
         <a href="mailto:istap9887@hiof.no">istap9887@hiof.no</a>
         <h2>Studie: Enkeltemner</h2>
         <h5>(Starter Informasjonssystemer høsten 2026)</h5 >
